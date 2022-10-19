d30b93f0cd819970e9810df572e1286bee2c560a
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-19 14:14:33 -0300

    feat: Implementation of the logic to insert publish content into table 'posts' and used hashtags into table 'hashtags'

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -2,8 +2,9 @@ import { connection } from '../db/db.js';
 import { postSchema } from '../schemas/postSchema.js';
 
 async function sendPost(req, res) {
-	const { userId, link, content } = req.body;
+	const { link, content } = req.body;
 	const validation = postSchema.validate(req.body);
+	const { session } = res.locals;
 
 	if (validation.error) {
 		const errors = validation.error.details
@@ -14,8 +15,45 @@ async function sendPost(req, res) {
 			.send(`The following errors an occurred:\n\n${errors}`);
 	}
 
-	const hashtags = content.split(' ').filter((word) => word[0] === '#');
-	return res.send(hashtags);
+	try {
+		if (content) {
+			const hashtable = content
+				.split(' ')
+				.filter((word) => word[0] === '#')
+				.forEach((element) => (hashtable[element] = true));
+			let valuesString = '';
+			for (let i = 1; i <= Object.keys(hashtable).length; i++) {
+				valuesString += `($${i}), `;
+			}
+			valuesString = valuesString.trim().replace(/.$/, '');
+
+			// await connection.query(
+			// 	'INSERT INTO posts (content, link, "userId") VALUES($1, $2, $3)',
+			// 	[content, link, session.userId]
+			// );
+
+			// const hashtags = content.split(' ').filter((word) => word[0] === '#');
+			// let valuesString = '';
+			// for (let i = 1; i <= hashtags.length; i++) {
+			// 	valuesString += `($${i}), `;
+			// }
+			// valuesString = valuesString.trim().replace(/.$/, '');
+
+			// await connection.query(
+			// 	`INSERT INTO hashtags (hashtag) VALUES ${valuesString}`,
+			// 	hashtags
+			// );
+			res.status(201).send({ valuesString, hashtags: Object.keys(hashtable) });
+		} else {
+			await connection.query(
+				'INSERT INTO posts (link, "userId") VALUES($1, $2)',
+				[link, session.userId]
+			);
+			res.statusSend(201);
+		}
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
 }
 
 export { sendPost };
--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -1,7 +1,6 @@
 import joi from 'joi';
 
 const postSchema = joi.object({
-	userId: joi.number().integer().required(),
 	link: joi
 		.string()
 		.pattern(/(^|\s)((https?:\/\/)[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/)
