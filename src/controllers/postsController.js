7e7112442e92cc283565d2351ac005166285cbd1
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-19 20:34:51 -0300

    feat: Implemented data insertion in 'postsHashtags' table, with 'postId' and 'hashtagId'. As well allow users publish contents without hashtag.

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -27,30 +27,44 @@ async function sendPost(req, res) {
 				valuesString += `($${i}), `;
 			}
 			valuesString = valuesString.trim().replace(/.$/, '');
+			const hashtags = Object.keys(hashtagsHashtable);
 
-			// await connection.query(
-			// 	'INSERT INTO posts (content, link, "userId") VALUES($1, $2, $3)',
-			// 	[content, link, session.userId]
-			// );
+			if (hashtags.length === 0) {
+				await connection.query(
+					'INSERT INTO posts (content, link, "userId") VALUES($1, $2, $3)',
+					[content, link, session.userId]
+				);
 
-			// const hashtags = content.split(' ').filter((word) => word[0] === '#');
-			// let valuesString = '';
-			// for (let i = 1; i <= hashtags.length; i++) {
-			// 	valuesString += `($${i}), `;
-			// }
-			// valuesString = valuesString.trim().replace(/.$/, '');
+				return res.sendStatus(201);
+			}
 
-			// await connection.query(
-			// 	`INSERT INTO hashtags (hashtag) VALUES ${valuesString}`,
-			// 	hashtags
-			// );
-			res
-				.status(201)
-				.send({
-					valuesString,
-					hashtags: Object.keys(hashtagsHashtable),
-					content,
-				});
+			const insertedPostId = await connection.query(
+				'INSERT INTO posts (content, link, "userId") VALUES($1, $2, $3) RETURNING id',
+				[content, link, session.userId]
+			);
+
+			const insertedHashtagsId = await connection.query(
+				`INSERT INTO hashtags (hashtag) VALUES ${valuesString} RETURNING id`,
+				hashtags
+			);
+
+			const hashtagsIdList = insertedHashtagsId.rows.map(
+				(element) => element.id
+			);
+			valuesString = '';
+			for (let i = 1; i <= hashtagsIdList.length; i++) {
+				valuesString += `($1, $${i+1}), `;
+			}
+			valuesString = valuesString.trim().replace(/.$/, '');
+
+			console.log([insertedPostId.rows[0].id, ...hashtagsIdList], valuesString);
+
+			await connection.query(
+				`INSERT INTO "postsHashtags" ("postId", "hashtagId") VALUES ${valuesString}`,
+				[insertedPostId.rows[0].id, ...hashtagsIdList]
+			);
+
+			res.sendStatus(201);
 		} else {
 			await connection.query(
 				'INSERT INTO posts (link, "userId") VALUES($1, $2)',
--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import * as postsController from '../controllers/postsController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/posts/publish', postsController.sendPost);
+router.post('/posts/publish', authMiddleware, postsController.sendPost);
 
 export default router;
