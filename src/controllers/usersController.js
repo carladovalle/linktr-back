611c7ec6cedbc459940f360656ebee156ccd8eee
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-22 02:53:11 -0300

    feat: Added id validation, returning error if it doesn't exist

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -20,7 +20,13 @@ async function getUserById(req, res) {
 	const { id } = req.params;
 
 	try {
+		const user = await connection.query('SELECT * FROM users WHERE id = $1', [
+			Number(id),
+		]);
 
+		if (user.rows.length === 0) {
+			return res.status(404).send('There are no users with this id.');
+		}
 
 		const userPosts = await connection.query(
 			`
