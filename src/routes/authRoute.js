51df3b665acb3c89d28c49e8bb745eb7ad0ab21c
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-23 02:56:28 -0300

    feat: Implemented authentication route, to guarantee safe access to all pages.

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/authController.js";
+import { allowUserAccess, login, register } from "../controllers/authController.js";
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
 router.post("/sign-up",register)
 router.post("/sign-in", login)
+router.get("/access/auth", authMiddleware, allowUserAccess)
 
 export default router
