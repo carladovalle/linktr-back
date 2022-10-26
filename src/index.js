5c053a52a8e6b189ab0fda33a793c56605a17366
Ronaldo <ronaldoandrade966@gmail.com>
2022-10-25 23:55:35 -0300

    follow/unfollow profile

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/authRoute.js';
 import postsRoute from './routes/postsRoute.js';
 import usersRoute from './routes/usersRoute.js';
 import likesRoute from './routes/likesRoute.js';
+import followsRoute from './routes/followsRoute.js';
 import hashtagRoutes from './routes/hashtagsRoute.js';
 import dotenv from 'dotenv';
 import { authMiddleware } from './middleware/authMiddleware.js';
@@ -45,6 +46,7 @@ server.use(postsRoute);
 server.use(usersRoute);
 server.use(likesRoute);
 server.use(hashtagRoutes);
+server.use(followsRoute);
 
 server.listen(process.env.PORT, () =>
 	console.log(`A mágica acontece no ${process.env.PORT}`)
