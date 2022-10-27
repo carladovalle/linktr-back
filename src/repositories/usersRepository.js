4e93b4d252c583b24b5f0d7d24e4bd703ab8d1f4
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-27 19:14:20 -0300

    refactor: Refactored functions on timeline, userPage and hashtagPage to fix infinite scroll.

--- src/repositories/usersRepository.js
+++ src/repositories/usersRepository.js
@@ -21,6 +21,12 @@ async function findUserById(id) {
 	return connection.query('SELECT * FROM users WHERE id = $1', [Number(id)]);
 }
 
+async function getUserInfo(id) {
+	return connection.query('SELECT id, name, image FROM users WHERE id = $1', [
+		Number(id),
+	]);
+}
+
 async function listUserPosts(id, offset, limit) {
 	return connection.query(
 		`
@@ -33,7 +39,7 @@ async function listUserPosts(id, offset, limit) {
 		FROM posts
 		JOIN metadatas 
 			ON posts.id = metadatas."postId"
-		RIGHT JOIN users 
+		JOIN users 
 			ON posts."userId" = users.id
 		WHERE users.id = $1
 		GROUP BY posts.id, users.name, users.image, users.id, metadatas.url, metadatas.title, metadatas.image, metadatas.description
@@ -44,4 +50,4 @@ async function listUserPosts(id, offset, limit) {
 	);
 }
 
-export { findUsers, findUserById, listUserPosts };
+export { findUsers, findUserById, listUserPosts, getUserInfo };
