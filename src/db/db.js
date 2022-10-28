09691ca769a55a2fe67a6d7786df81262ab176d2
Victor Hugo de Souza <98493760+Viktorhs@users.noreply.github.com>
2022-10-28 01:55:20 -0300

    update

--- src/db/db.js
+++ src/db/db.js
@@ -4,11 +4,12 @@ import pg from 'pg'
 
 const { Pool } = pg
 
-const connection = new Pool({
+
+  const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:{
    rejectUnauthorized: false
- }
-});
+  }
+  });
 
 export { connection }
